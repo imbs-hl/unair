1d7645a749296bbf3665b914505e67469a84e678
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2022-05-11 12:21:49 +0200

    Source files

---
+++ R/resume_results.R
@@ -0,0 +1,22 @@
+## Global initialization
+source("init-global.R", chdir = TRUE)
+
+
+## Resume results for null case distribution of importance estimates
+working_dir <- file.path(main_dir, "02-dist_null_case")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("res_compare_dist_null.R", chdir = TRUE)
+source("res_type1.R", chdir = TRUE)
+
+## Power study
+working_dir <- file.path(main_dir, "03-dist_alternativ_case")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("res_simulate_effect.R", chdir = TRUE)
+
+## Real data application
+working_dir <- file.path(main_dir, "04-real_data_applications")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("res_golub.R", chdir = TRUE)
---
+++ R/start_jobs.R
@@ -0,0 +1,32 @@
+## Global initialization
+source("init-global.R", chdir = TRUE)
+
+## Data download and preprocessing
+working_dir <- file.path(main_dir, "01-preprocessing")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("01-breast_cancer.R", chdir = TRUE)
+source("02-cervival_cancer.R", chdir = TRUE)
+source("03-gene_expr_rna.R", chdir = TRUE)
+source("05-heart_failure.R", chdir = TRUE)
+source("06-mice-protein.R", chdir = TRUE)
+source("07-wine.R", chdir = TRUE)
+
+## Compute null case distribution of importance estimates
+working_dir <- file.path(main_dir, "02-dist_null_case")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("run_compare_dist_null.R", chdir = TRUE)
+source("type1.R", chdir = TRUE)
+
+## Power study
+working_dir <- file.path(main_dir, "03-dist_alternativ_case")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("run_simulate_effect.R", chdir = TRUE)
+
+## Real data application
+working_dir <- file.path(main_dir, "04-real_data_applications")
+setwd(working_dir, chdir = TRUE)
+source("init.R", chdir = TRUE)
+source("03-golub.R", chdir = TRUE)

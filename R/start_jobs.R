2a5fa93b56b81c97c5f2c2edd100a79026e07950
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2022-05-11 16:17:48 +0200

    Firstly set batchtools configuration files

--- R/start_jobs.R
+++ R/start_jobs.R
@@ -1,13 +1,25 @@
 ## Global initialization
 source("init-global.R", chdir = TRUE)
 
+## =============================================================================
+## !!!!!!!!!!!!!!!!!!! Please set these variable before !!!!!!!!!!!!!!!!!!!!! ##
+## =============================================================================
+##
+## For resource manager, e.g. SLURM
+partition <- "prio" ## Should be the name of the queue
+account <- "dzhkomics" ## Should your slurm account
+
+## Batchtools configuration file
+config_file <- file.path(main_dir, "99_batchtools/batchtools.conf.R")
+
+
 ## Data download and preprocessing
 working_dir <- file.path(main_dir, "01-preprocessing")
 setwd(working_dir)
 source("init.R", chdir = TRUE)
 source("01-breast_cancer.R", chdir = TRUE)
 source("02-cervival_cancer.R", chdir = TRUE)
-source("03-gene_expr_rna.R", chdir = TRUE)
+source("03-gene_exp_rna.R", chdir = TRUE)
 source("05-heart_failure.R", chdir = TRUE)
 source("06-mice-protein.R", chdir = TRUE)
 source("07-wine.R", chdir = TRUE)
